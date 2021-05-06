4920d542a10cdbf071ce047736bb2a9d6b1448a5
Aleksandr S <ardhead@gmail.com>
2021-05-06 19:19:39 +0300

    commit test 2

--- contracts/mainnet/connectors/uniswap/events.sol
+++ contracts/mainnet/connectors/uniswap/events.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.7.0;
 
 contract Events {
     event LogDepositLiquidity(
-        address indexed tokenA,
         address indexed tokenB,
         uint256 amtA,
         uint256 amtB,
+        uint256 uniAmount,
         uint256 getId,
         uint256 setId
     );
