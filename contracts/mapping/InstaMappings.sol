247d75f857ea93d63a61db874d5be4fd17521f6b
Daksh Miglani <hello@dak.sh>
2021-05-09 21:00:29 +0530

    feat: 🎸 add new access control and use it in InstaMappings

--- contracts/mapping/InstaMappings.sol
+++ contracts/mapping/InstaMappings.sol
@@ -1,32 +1,17 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
-import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import {AccessControl} from "./AccessControl.sol";
+
 interface IndexInterface {
     function master() external view returns (address);
 }
 
 contract InstaMappings is AccessControl {
-    IndexInterface public constant instaIndex = IndexInterface(0x2971AdFa57b20E5a416aE5a708A8655A9c74f723);
+    IndexInterface public constant instaIndex =
+        IndexInterface(0x2971AdFa57b20E5a416aE5a708A8655A9c74f723);
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-
-    function setMaster() public {
-        require(msg.sender == address(this), "msg.sender is not this contract");
-        uint256 adminCount = getRoleMemberCount(DEFAULT_ADMIN_ROLE);
-        require(adminCount == 1, "setMaster::Wrong-admin-count");
-        address currentMaster = getRoleMember(DEFAULT_ADMIN_ROLE, 0);
-        address master = instaIndex.master();
-        
-        if (currentMaster != master) {
-            grantRole(DEFAULT_ADMIN_ROLE, master);
-            revokeRole(DEFAULT_ADMIN_ROLE, currentMaster);
-        }
-
-        adminCount = getRoleMemberCount(DEFAULT_ADMIN_ROLE);
-        require(adminCount == 1, "setMaster::Wrong-admin-count");
-        require(hasRole(DEFAULT_ADMIN_ROLE, master), "setMaster::InstaIndex-master-not-set");
-    }
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, instaIndex.master());
@@ -34,13 +19,21 @@ contract InstaMappings is AccessControl {
         _setupRole(ADMIN_ROLE, address(this));
     }
 
-    function getMappingContractRole(address mappingContract) public pure returns (bytes32 role){
+    function getMappingContractRole(address mappingContract)
+        public
+        pure
+        returns (bytes32 role)
+    {
         assembly {
             role := mload(add(mappingContract, 32))
         }
     }
 
-    function hasRole(address mappingAddr, address account) public view returns (bool) {
+    function hasRole(address mappingAddr, address account)
+        public
+        view
+        returns (bool)
+    {
         return super.hasRole(getMappingContractRole(mappingAddr), account);
     }
 
