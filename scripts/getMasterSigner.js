47ea4ffdff1d8a6cc143c01060c5b29d1ff31e97
Daksh Miglani <hello@dak.sh>
2021-06-04 16:17:16 +0530

    perf: ⚡️ add utils for adding liq, impersonating and master sign

--- scripts/getMasterSigner.js
+++ scripts/getMasterSigner.js
@@ -1,23 +1,25 @@
 const hre = require("hardhat");
-const { ethers, waffle } = hre;
+const { ethers } = hre;
 const addresses = require("./constant/addresses");
 const abis = require("./constant/abis");
-const { provider, deployContract } = waffle
 
+module.exports = async function() {
+  const [_, __, ___, wallet3] = await ethers.getSigners();
+  const instaIndex = new ethers.Contract(
+    addresses.core.instaIndex,
+    abis.core.instaIndex,
+    wallet3
+  );
 
-module.exports = async function () {
-    const instaIndex = await ethers.getContractAt(abis.core.instaIndex, addresses.core.instaIndex)
+  const masterAddress = await instaIndex.master(); // TODO: make it constant?
+  await hre.network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [masterAddress],
+  });
+  await wallet3.sendTransaction({
+    to: masterAddress,
+    value: ethers.utils.parseEther("10"),
+  });
 
-    const masterAddress = await instaIndex.master(); // TODO: make it constant?
-    await hre.network.provider.request({
-        method: "hardhat_impersonateAccount",
-        params: [ masterAddress]
-    })
-    const [wallet0, wallet1, wallet2, wallet3] = await ethers.getSigners()
-    await wallet3.sendTransaction({
-        to: masterAddress,
-        value: ethers.utils.parseEther("10")
-      });
-
-    return await ethers.getSigner(masterAddress);
+  return await ethers.getSigner(masterAddress);
 };
