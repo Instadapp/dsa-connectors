c0875a4b0f3cc7f34259aeea9a36d1b95a096891
Thrilok Kumar <thrilok2000@gmail.com>
2021-06-18 14:27:19 +0530

    Making wethContract as constant

--- contracts/mainnet/connectors/weth/helpers.sol
+++ contracts/mainnet/connectors/weth/helpers.sol
@@ -4,5 +4,5 @@ import { TokenInterface } from "../../common/interfaces.sol";
 
 
 abstract contract Helpers  {
-    TokenInterface wethContract = TokenInterface(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    TokenInterface constant public wethContract = TokenInterface(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 }
