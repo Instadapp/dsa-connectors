40a09364084c7e838fcd38fc9eef67d9c1459438
Samyak Jain <34437877+KaymasJain@users.noreply.github.com>
2021-07-25 00:36:21 +0530

    minor fixes

--- contracts/mainnet/connectors/automated_defi_limit_order/helpers.sol
+++ contracts/mainnet/connectors/automated_defi_limit_order/helpers.sol
@@ -1,11 +1,9 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
-import { DSMath } from "../../common/math.sol";
 import { Basic } from "../../common/basic.sol";
-import { ComptrollerInterface, COMPInterface, CompoundMappingInterface } from "./interface.sol";
 
-abstract contract Helpers is DSMath, Basic {
+abstract contract Helpers is Basic {
     /**
      * @dev Compound Comptroller
      */
