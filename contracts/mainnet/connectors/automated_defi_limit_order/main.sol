c0370a7b7a37bcb3a24b26deb0e11a44c7f7b45d
Samyak Jain <34437877+KaymasJain@users.noreply.github.com>
2021-08-18 02:46:31 +0530

    minor bugs fixing

--- contracts/mainnet/connectors/automated_defi_limit_order/main.sol
+++ contracts/mainnet/connectors/automated_defi_limit_order/main.sol
@@ -66,8 +66,6 @@ abstract contract CompResolver is Events, Helpers {
         uint sellAmt,
         uint unitAmt
     ) internal view returns (uint _slippageAmt) {
-        TokenInterface buyToken = TokenInterface(buyAddr);
-        TokenInterface sellToken = TokenInterface(sellAddr);
         (uint _buyDec, uint _sellDec) = getTokensDec(buyToken, sellToken);
         uint _sellAmt18 = convertTo18(_sellDec, sellAmt);
         _slippageAmt = convert18ToDec(_buyDec, wmul(unitAmt, _sellAmt18));
@@ -97,6 +95,7 @@ abstract contract CompResolver is Events, Helpers {
         address buyAddr,
         address sellAddr,
         uint sellAmt,
+        uint unitAmt,
         bytes8[] memory orderIds,
         uint[] memory distributions,
         uint units,
@@ -106,27 +105,18 @@ abstract contract CompResolver is Events, Helpers {
         string memory _eventName,
         bytes memory _eventParam
     ) {
+        sellAmt = getUint(getId, sellAmt);
+
         uint _slippageAmt = _calSlippageCheck(TokenInterface(buyAddr), TokenInterface(sellAddr), sellAmt, unitAmt);
 
         uint buyAmt = limitOrderContract.sell(sellAddr, buyAddr, sellAmt, _slippageAmt, orderIds, distributions, units, address(this));
+
+        setUint(setId, buyAmt);
 
         _eventName = "LogSell(address,address,uint256,uint256,uint256,uint256)";
         _eventParam = abi.encode(buyAddr, sellAddr, buyAmt, sellAmt, 0, setId);
     }
 
-    /**
-     * @dev Delegate votes.
-     * @notice Delegate votes.
-     * @param delegatee The address to delegate the votes to.
-    */
-    function delegate(address delegatee) external payable returns (string memory _eventName, bytes memory _eventParam) {
-        require(compToken.delegates(address(this)) != delegatee, "Already delegated to same delegatee.");
-
-        compToken.delegate(delegatee);
-
-        _eventName = "LogDelegate(address)";
-        _eventParam = abi.encode(delegatee);
-    }
 }
 
 contract ConnectV2DefiLimitOrders is CompResolver {
