e50bb11bd4199071d4ce9d9e5f2ae44d251a7de1
Thomas Bouder <tbouder@protonmail.com>
2021-08-23 23:35:31 +0200

    ✨ Fixing doc issues & payable

--- contracts/mainnet/connectors/yearn_v2/main.sol
+++ contracts/mainnet/connectors/yearn_v2/main.sol
@@ -13,6 +13,7 @@ import { YearnV2Interface } from "./interface.sol";
 abstract contract YearnResolver is Events, Basic {
     /**
      * @dev Deposit funds in the vault, issuing shares to recipient.
+     * @notice This will deposit funds to a specific Yearn Vault.
      * @param vault The address of the vault to deposit funds into.
      * @param amt The amount of tokens to deposit.
      * @param getId ID to retrieve amtA.
@@ -23,11 +24,11 @@ abstract contract YearnResolver is Events, Basic {
         uint256 amt,
         uint256 getId,
         uint256 setId
-    ) external returns (string memory _eventName, bytes memory _eventParam) {
+    ) external payable returns (string memory _eventName, bytes memory _eventParam) {
         uint _amt = getUint(getId, amt);
 
         YearnV2Interface yearn = YearnV2Interface(vault);
-        
+
         address want = yearn.token();
 
         TokenInterface tokenContract = TokenInterface(want);
@@ -44,6 +45,7 @@ abstract contract YearnResolver is Events, Basic {
 
     /**
      * @dev Withdraw shares from the vault.
+     * @notice This will withdraw the share from a specific Yearn Vault.
      * @param vault The address of the vault to withdraw shares from.
      * @param amt The amount of shares to withdraw.
      * @param getId ID to retrieve amtA.
@@ -54,7 +56,7 @@ abstract contract YearnResolver is Events, Basic {
         uint256 amt,
         uint256 getId,
         uint256 setId
-    ) external returns (string memory _eventName, bytes memory _eventParam) {
+    ) external payable returns (string memory _eventName, bytes memory _eventParam) {
         uint _amt = getUint(getId, amt);
 
         YearnV2Interface yearn = YearnV2Interface(vault);
