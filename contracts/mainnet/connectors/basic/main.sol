4d1af0fe3efc1cc0a4a40053bf01b68787eead9a
Thrilok kumar <thrilok2000@gmail.com>
2021-08-26 01:23:12 +0530

    Update main.sol

--- contracts/mainnet/connectors/basic/main.sol
+++ contracts/mainnet/connectors/basic/main.sol
@@ -18,7 +18,7 @@ abstract contract BasicResolver is Events, DSMath, Basic {
     /**
      * @dev Deposit Assets To Smart Account.
      * @notice Deposit a token to DSA. 
-     * @param token The address of the token to deposit.\n(For ETH: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE and need to pass `value` parameter equal to `amt` in cast function ```dsa.cast({..., value: amt})```.\nFor ERC20: Need to give allowance piror casting spells.)
+     * @param token The address of the token to deposit.<br>(For <b>ETH</b>: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE and need to pass `value` parameter equal to `amt` in cast function ```dsa.cast({..., value: amt})```.<br>For <b>ERC20</b>: Need to give allowance piror casting spells.)
      * @param amt The amount of tokens to deposit. (For max: `uint256(-1)` (Not valid for ETH))
      * @param getId ID to retrieve amt.
      * @param setId ID stores the amount of tokens deposited.
