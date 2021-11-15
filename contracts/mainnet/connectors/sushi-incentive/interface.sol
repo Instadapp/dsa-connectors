b3c25652650661073bbf5e0316a6cec88e853f2f
cryptoDev222 <genius.developer.63@gmail.com>
2021-11-15 08:58:14 -0600

    minor fixes

--- contracts/mainnet/connectors/sushi-incentive/interface.sol
+++ contracts/mainnet/connectors/sushi-incentive/interface.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
-import "./libraries/IERC20.sol";
 
 struct UserInfo {
     uint256 amount;
@@ -12,6 +11,30 @@ struct PoolInfo {
     uint256 allocPoint; // How many allocation points assigned to this pool. SUSHIs to distribute per block.
     uint256 lastRewardBlock; // Last block number that SUSHIs distribution occurs.
     uint256 accSushiPerShare; // Accumulated SUSHIs per share, times 1e12. See below.
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /// @notice EIP 2612
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
 }
 
 interface IMasterChef {
