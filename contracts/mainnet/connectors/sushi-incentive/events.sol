bc37392f49dd41e971bed5b760aa8e89de002e6c
cryptoDev222 <genius.developer.63@gmail.com>
2021-11-15 08:58:14 -0600

    minor fixes

--- contracts/mainnet/connectors/sushi-incentive/events.sol
+++ contracts/mainnet/connectors/sushi-incentive/events.sol
@@ -2,32 +2,27 @@ pragma solidity ^0.7.0;
 
 contract Events {
     event LogDeposit(
-        address indexed user,
         uint256 indexed pid,
         uint256 indexed version,
         uint256 amount
     );
     event LogWithdraw(
-        address indexed user,
         uint256 indexed pid,
         uint256 indexed version,
         uint256 amount
     );
     event LogEmergencyWithdraw(
-        address indexed user,
         uint256 indexed pid,
         uint256 indexed version,
         uint256 lpAmount,
         uint256 rewardsAmount
     );
     event LogHarvest(
-        address indexed user,
         uint256 indexed pid,
         uint256 indexed version,
         uint256 amount
     );
     event LogWithdrawAndHarvest(
-        address indexed user,
         uint256 indexed pid,
         uint256 indexed version,
         uint256 widrawAmount,
