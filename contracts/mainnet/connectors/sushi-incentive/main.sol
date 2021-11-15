627e839c70a676ff67f31d2a31a9661d59985c69
cryptoDev222 <genius.developer.63@gmail.com>
2021-11-15 08:58:14 -0600

    minor fixes

--- contracts/mainnet/connectors/sushi-incentive/main.sol
+++ contracts/mainnet/connectors/sushi-incentive/main.sol
@@ -13,6 +13,7 @@ import {Events} from "./events.sol";
 abstract contract SushipswapIncentiveResolver is Helpers, Events {
     /**
      * @dev deposit LP token to masterChef
+     * @notice deposit LP token to masterChef
      * @param token1 token1 of LP token
      * @param token2 token2 of LP token
      * @param amount amount of LP token
@@ -25,7 +26,13 @@ abstract contract SushipswapIncentiveResolver is Helpers, Events {
         uint256 amount,
         uint256 getId,
         uint256 setId
-    ) external {
+    )
+        external
+        payable
+        returns (string memory _eventName, bytes memory _eventParam)
+    {
+        token1 = changeEthAddrToWethAddr(token1);
+        token2 = changeEthAddrToWethAddr(token2);
         amount = getUint(getId, amount);
         (uint256 _pid, uint256 _version, address lpTokenAddr) = _getPoolId(
             token1,
@@ -36,11 +43,13 @@ abstract contract SushipswapIncentiveResolver is Helpers, Events {
         TokenInterface lpToken = TokenInterface(lpTokenAddr);
         lpToken.approve(address(masterChef), amount);
         _deposit(_pid, amount, _version);
-        emit LogDeposit(address(this), _pid, _version, amount);
+        _eventName = "LogDeposit(uint256,uint256,uint256)";
+        _eventParam = abi.encode(_pid, _version, amount);
     }
 
     /**
      * @dev withdraw LP token from masterChef
+     * @notice withdraw LP token from masterChef
      * @param token1 token1 of LP token
      * @param token2 token2 of LP token
      * @param amount amount of LP token
@@ -53,17 +62,25 @@ abstract contract SushipswapIncentiveResolver is Helpers, Events {
         uint256 amount,
         uint256 getId,
         uint256 setId
-    ) external {
+    )
+        external
+        payable
+        returns (string memory _eventName, bytes memory _eventParam)
+    {
+        token1 = changeEthAddrToWethAddr(token1);
+        token2 = changeEthAddrToWethAddr(token2);
         amount = getUint(getId, amount);
         (uint256 _pid, uint256 _version, ) = _getPoolId(token1, token2);
         setUint(setId, _pid);
         require(_pid != uint256(-1), "pool-does-not-exist");
         _withdraw(_pid, amount, _version);
-        emit LogWithdraw(address(this), _pid, _version, amount);
+        _eventName = "LogWithdraw(uint256,uint256,uint256)";
+        _eventParam = abi.encode(_pid, _version, amount);
     }
 
     /**
      * @dev harvest from masterChef
+     * @notice harvest from masterChef
      * @param token1 token1 deposited of LP token
      * @param token2 token2 deposited LP token
      * @param setId ID stores Pool ID
@@ -72,18 +89,26 @@ abstract contract SushipswapIncentiveResolver is Helpers, Events {
         address token1,
         address token2,
         uint256 setId
-    ) external {
+    )
+        external
+        payable
+        returns (string memory _eventName, bytes memory _eventParam)
+    {
+        token1 = changeEthAddrToWethAddr(token1);
+        token2 = changeEthAddrToWethAddr(token2);
         (uint256 _pid, uint256 _version, ) = _getPoolId(token1, token2);
         setUint(setId, _pid);
         require(_pid != uint256(-1), "pool-does-not-exist");
         (, uint256 rewardsAmount) = _getUserInfo(_pid, _version);
         if (_version == 2) _harvest(_pid);
         else _withdraw(_pid, 0, _version);
-        emit LogHarvest(address(this), _pid, _version, rewardsAmount);
+        _eventName = "LogHarvest(uint256,uint256,uint256)";
+        _eventParam = abi.encode(_pid, _version, rewardsAmount);
     }
 
     /**
      * @dev withdraw LP token and harvest from masterChef
+     * @notice withdraw LP token and harvest from masterChef
      * @param token1 token1 of LP token
      * @param token2 token2 of LP token
      * @param amount amount of LP token
@@ -96,24 +121,26 @@ abstract contract SushipswapIncentiveResolver is Helpers, Events {
         uint256 amount,
         uint256 getId,
         uint256 setId
-    ) external {
+    )
+        external
+        payable
+        returns (string memory _eventName, bytes memory _eventParam)
+    {
+        token1 = changeEthAddrToWethAddr(token1);
+        token2 = changeEthAddrToWethAddr(token2);
         amount = getUint(getId, amount);
         (uint256 _pid, uint256 _version, ) = _getPoolId(token1, token2);
         setUint(setId, _pid);
         require(_pid != uint256(-1), "pool-does-not-exist");
         (, uint256 rewardsAmount) = _getUserInfo(_pid, _version);
         _withdrawAndHarvest(_pid, amount, _version);
-        emit LogWithdrawAndHarvest(
-            address(this),
-            _pid,
-            _version,
-            amount,
-            rewardsAmount
-        );
+        _eventName = "LogWithdrawAndHarvest(uint256,uint256,uint256,uint256)";
+        _eventParam = abi.encode(_pid, _version, amount, rewardsAmount);
     }
 
     /**
      * @dev emergency withdraw from masterChef
+     * @notice emergency withdraw from masterChef
      * @param token1 token1 deposited of LP token
      * @param token2 token2 deposited LP token
      * @param setId ID stores Pool ID
@@ -122,7 +149,13 @@ abstract contract SushipswapIncentiveResolver is Helpers, Events {
         address token1,
         address token2,
         uint256 setId
-    ) external {
+    )
+        external
+        payable
+        returns (string memory _eventName, bytes memory _eventParam)
+    {
+        token1 = changeEthAddrToWethAddr(token1);
+        token2 = changeEthAddrToWethAddr(token2);
         (uint256 _pid, uint256 _version, ) = _getPoolId(token1, token2);
         setUint(setId, _pid);
         require(_pid != uint256(-1), "pool-does-not-exist");
@@ -131,13 +164,8 @@ abstract contract SushipswapIncentiveResolver is Helpers, Events {
             _version
         );
         _emergencyWithdraw(_pid, _version);
-        emit LogEmergencyWithdraw(
-            address(this),
-            _pid,
-            _version,
-            lpAmount,
-            rewardsAmount
-        );
+        _eventName = "LogEmergencyWithdraw(uint256,uint256,uint256,uint256)";
+        _eventParam = abi.encode(_pid, _version, lpAmount, rewardsAmount);
     }
 }
 
