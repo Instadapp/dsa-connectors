4736e8dadc0147454a23567d0c183e9c39b995e4
pradyuman-verma <pradyumnverma27@gmail.com>
2021-11-19 02:04:28 +0530

    feat: sample uniswap connector on arbitrum

---
+++ test/sample/sample.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+const hre = require("hardhat");
+const { web3, deployments, waffle, ethers } = hre;
+const { provider, deployContract } = waffle;
+
+const deployAndEnableConnector = require("../../scripts/deployAndEnableConnector.js");
+const buildDSAv2 = require("../../scripts/buildDSAv2");
+const encodeSpells = require("../../scripts/encodeSpells.js");
+
+desribe("SAMPLE UNISWAP", () => {
+  it("X", async () => {});
+});
