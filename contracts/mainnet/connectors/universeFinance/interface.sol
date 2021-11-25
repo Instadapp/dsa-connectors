fcec1b0dbe0dbd872651011894cfa1f690f4c970
UniverseFinance <87629076+UniverseFinance@users.noreply.github.com>
2021-11-25 13:21:25 +0800

    Update interface.sol

--- contracts/mainnet/connectors/universeFinance/interface.sol
+++ contracts/mainnet/connectors/universeFinance/interface.sol
@@ -1,4 +1,7 @@
 pragma solidity ^0.7.6;
+
+import "../../common/interfaces.sol";
+
 pragma abicoder v2;
 
 interface IUniverseAdapter {
@@ -12,6 +15,10 @@ interface IUniverseAdapter {
 }
 
 interface IVaultV3 {
+
+    function token0() external returns(TokenInterface);
+
+    function token1() external returns(TokenInterface);
 
     function withdraw(uint256 share0, uint256 share1) external returns(uint256, uint256);
 }
