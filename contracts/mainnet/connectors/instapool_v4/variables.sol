929e2bd8f0e55eaa2c3c94e3dad68416a930233f
Samyak Jain <34437877+KaymasJain@users.noreply.github.com>
2021-11-30 04:11:00 +0530

    added the proxy address in flashloan connector

--- contracts/mainnet/connectors/instapool_v4/variables.sol
+++ contracts/mainnet/connectors/instapool_v4/variables.sol
@@ -8,6 +8,6 @@ contract Variables {
     /**
     * @dev Instapool contract proxy
     */
-    InstaFlashV4Interface public constant instaPool = InstaFlashV4Interface(address(0)); // TODO: update address
+    InstaFlashV4Interface public constant instaPool = InstaFlashV4Interface(0x619Ad2D02dBeE6ebA3CDbDA3F98430410e892882);
 
 }
