c2dfae64387c8fb4ab249fb34a5b021b0e7f1000
pradyuman-verma <pradyumnverma27@gmail.com>
2021-12-06 01:34:41 +0530

    refactored scripts/*

--- scripts/tests/buildDSAv2.ts
+++ scripts/tests/buildDSAv2.ts
@@ -1,13 +1,23 @@
 import { ethers } from "hardhat";
 
-import { addresses } from "../constant/addresses";
+const network = String(process.env.networkType);
+
+import { addressesPolygon } from "./polygon/addressesPolygon";
+import { addresses } from "./mainnet/addresses";
 import { abis } from "../constant/abis";
 import { abi } from "../../deployements/mainnet/Implementation_m1.sol/InstaImplementationM1.json";
+
+function getAddress(network: string | undefined) {
+  if (network === "polygon") return addressesPolygon.core.instaIndex;
+  // else if (network === "arbitrum") return addressesPolygon.core.instaIndex;
+  // else if (network === "avalanche") return addressesPolygon.core.instaIndex;
+  else return addresses.core.instaIndex;
+}
 
 export async function buildDSAv2(owner: any) {
   const instaIndex = await ethers.getContractAt(
     abis.core.instaIndex,
-    addresses.core.instaIndex
+    getAddress(String(process.env.networkType))
   );
 
   const tx = await instaIndex.build(owner, 2, owner);
--- scripts/tests/deployAndEnableConnector.ts
+++ scripts/tests/deployAndEnableConnector.ts
@@ -1,10 +1,18 @@
-import { addresses } from "../constant/addresses";
+import { addressesPolygon } from "./polygon/addressesPolygon";
+import { addresses } from "./mainnet/addresses";
 import { abis } from "../constant/abis";
 
 // const { deployContract } = waffle;
 // import { ethers } from "hardhat";
 // import { promises as fs } from "fs";
 // import { deployContract } from "ethereum-waffle";
+
+function getAddress(network: string | undefined) {
+  if (network === "polygon") return addressesPolygon;
+  // else if (network === "arbitrum") return addressesPolygon;
+  // else if (network === "avalanche") return addressesPolygon;
+  else return addresses;
+}
 
 export async function deployAndEnableConnector({
   connectorName,
@@ -19,7 +27,8 @@ export async function deployAndEnableConnector({
     .connect(signer)
     .addConnectors([connectorName], [connectorInstanace.address]);
 
-  addresses.connectors[connectorName] = connectorInstanace.address;
+  getAddress(String(process.env.networkType)).connectors[connectorName] =
+    connectorInstanace.address;
   abis.connectors[connectorName] = contractArtifact.abi;
 
   return connectorInstanace;
