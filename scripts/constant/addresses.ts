c0f7261999e0c91fefc055408d67747dafa90072
pradyuman-verma <pradyumnverma27@gmail.com>
2021-12-08 13:13:27 +0530

    added multichain constants

--- scripts/constant/addresses.ts
+++ scripts/constant/addresses.ts
@@ -1,11 +1,41 @@
 export const addresses: Record<string, any> = {
-  connectors: {
-    basic: "0xe5398f279175962E56fE4c5E0b62dc7208EF36c6",
-    auth: "0xd1aff9f2acf800c876c409100d6f39aea93fc3d9",
-    "INSTAPOOL-A": "0x5806af7ab22e2916fa582ff05731bf7c682387b2",
+  mainnet: {
+    connectors: {
+      basic: "0xe5398f279175962E56fE4c5E0b62dc7208EF36c6",
+      auth: "0xd1aff9f2acf800c876c409100d6f39aea93fc3d9",
+      "INSTAPOOL-A": "0x5806af7ab22e2916fa582ff05731bf7c682387b2",
+    },
+    core: {
+      connectorsV2: "0x97b0B3A8bDeFE8cB9563a3c610019Ad10DB8aD11",
+      instaIndex: "0x2971AdFa57b20E5a416aE5a708A8655A9c74f723",
+    },
   },
-  core: {
-    connectorsV2: "0x97b0B3A8bDeFE8cB9563a3c610019Ad10DB8aD11",
-    instaIndex: "0x2971AdFa57b20E5a416aE5a708A8655A9c74f723",
+  polygon: {
+    connectors: {
+      basic: "0x6214f9c4F9700fc7a50B5f9aEEB819d647406Ac7",
+      auth: "0xD6daA927ad756a4022858dddcc4E26137b30DB4D",
+      "INSTAPOOL-A": "0x8f1e38c53af7bD2b2bE01b9580911b7Cca504F1b",
+    },
+    core: {
+      connectorsV2: "0x2A00684bFAb9717C21271E0751BCcb7d2D763c88",
+      instaIndex: "0xA9B99766E6C676Cf1975c0D3166F96C0848fF5ad",
+    },
   },
+  avalanche: {
+    connectors: {
+      // basic: "0x6214f9c4F9700fc7a50B5f9aEEB819d647406Ac7",
+      // auth: "0xD6daA927ad756a4022858dddcc4E26137b30DB4D",
+      // "INSTAPOOL-A": "0x8f1e38c53af7bD2b2bE01b9580911b7Cca504F1b",
+    },
+    core: {
+      connectorsV2: "0x67fCE99Dd6d8d659eea2a1ac1b8881c57eb6592B",
+      instaIndex: "0x1eE00C305C51Ff3bE60162456A9B533C07cD9288",
+    },
+  },
+  arbitrum: {
+    core: {
+      connectorsV2: "0x127d8cD0E2b2E0366D522DeA53A787bfE9002C14",
+      instaIndex: "0x6CE3e607C808b4f4C26B7F6aDAeB619e49CAbb25",
+    },
+  }
 };
--- scripts/constant/tokens.ts
+++ scripts/constant/tokens.ts
@@ -1,23 +1,71 @@
 export const tokens = {
-  eth: {
-    type: "token",
-    symbol: "ETH",
-    name: "Ethereum",
-    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-    decimals: 18,
+  mainnet: {
+    eth: {
+      type: "token",
+      symbol: "ETH",
+      name: "Ethereum",
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      decimals: 18,
+    },
+    dai: {
+      type: "token",
+      symbol: "DAI",
+      name: "DAI Stable",
+      address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+      decimals: 18,
+    },
+    usdc: {
+      type: "token",
+      symbol: "USDC",
+      name: "USD Coin",
+      address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      decimals: 6,
+    },
   },
-  dai: {
-    type: "token",
-    symbol: "DAI",
-    name: "DAI Stable",
-    address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-    decimals: 18,
+  polygon: {
+    eth: {
+      type: "token",
+      symbol: "ETH",
+      name: "Ethereum",
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      decimals: 18,
+    },
+    dai: {
+      type: "token",
+      symbol: "DAI",
+      name: "DAI Stable",
+      address: "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
+      decimals: 18,
+    },
+    usdc: {
+      type: "token",
+      symbol: "USDC",
+      name: "USD Coin",
+      address: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+      decimals: 6,
+    },
   },
-  usdc: {
-    type: "token",
-    symbol: "USDC",
-    name: "USD Coin",
-    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-    decimals: 6,
-  },
+  avalanche: {
+    eth: {
+      type: "token",
+      symbol: "ETH",
+      name: "Ethereum",
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      decimals: 18,
+    },
+    dai: {
+      type: "token",
+      symbol: "DAI",
+      name: "DAI Stable",
+      address: "0xd586e7f844cea2f87f50152665bcbc2c279d8d70",
+      decimals: 18,
+    },
+    usdc: {
+      type: "token",
+      symbol: "USDC",
+      name: "USD Coin",
+      address: "0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664",
+      decimals: 6,
+    },
+  }
 };
