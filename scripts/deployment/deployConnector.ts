fd9c72eafb1cb04d40187a825be8c3b71cd1afce
pradyuman-verma <pradyumnverma27@gmail.com>
2021-12-26 12:44:52 +0530

    updated deployConnectorFromCmd script + minor fixes

--- scripts/deployment/deployConnector.ts
+++ scripts/deployment/deployConnector.ts
@@ -1,6 +1,7 @@
 import { ethers } from "hardhat";
 
-export const deployConnector = async (connectorName: string) => {
+export const deployConnector = async (connectorName?: string) => {
+  connectorName = String(process.env.connectorName) ?? connectorName;
   const Connector = await ethers.getContractFactory(connectorName);
   const connector = await Connector.deploy();
   await connector.deployed();
