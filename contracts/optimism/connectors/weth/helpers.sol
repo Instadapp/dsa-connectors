cbe099c654ca02526622e9b6266b6f0c44056bec
Thrilok Kumar <thrilok2000@gmail.com>
2022-01-19 04:39:24 +0530

    Added TODO

--- contracts/optimism/connectors/weth/helpers.sol
+++ contracts/optimism/connectors/weth/helpers.sol
@@ -4,5 +4,5 @@ import { TokenInterface } from "../../common/interfaces.sol";
 
 abstract contract Helpers  {
     TokenInterface constant internal wethContract = TokenInterface(0x4200000000000000000000000000000000000006);
-    TokenInterface constant internal wethFixContract = TokenInterface(0x4200000000000000000000000000000000000006);
+    TokenInterface constant internal wethFixContract = TokenInterface(address(0x00)); // TODO: change it after deployment
 }
