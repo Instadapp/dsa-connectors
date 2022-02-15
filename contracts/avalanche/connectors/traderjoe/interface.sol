a58928429ebcb87e4504d557261d5127dd21537e
Croooook <rajatgoyal6404@gmail.com>
2022-02-15 16:24:35 +0530

    changes in interface.sol

--- contracts/avalanche/connectors/traderjoe/interface.sol
+++ contracts/avalanche/connectors/traderjoe/interface.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.7.0;
 
-interface CTokenInterface {
+interface JTokenInterface {
     function mint(uint mintAmount) external returns (uint);
     function redeem(uint redeemTokens) external returns (uint);
     function borrow(uint borrowAmount) external returns (uint);
     function repayBorrow(uint repayAmount) external returns (uint);
     function repayBorrowBehalf(address borrower, uint repayAmount) external returns (uint); // For ERC20
-    function liquidateBorrow(address borrower, uint repayAmount, address cTokenCollateral) external returns (uint);
+    function liquidateBorrow(address borrower, uint repayAmount, address jTokenCollateral) external returns (uint);
 
     function borrowBalanceCurrent(address account) external returns (uint);
     function redeemUnderlying(uint redeemAmount) external returns (uint);
@@ -15,22 +15,22 @@ interface CTokenInterface {
     function balanceOf(address owner) external view returns (uint256 balance);
 }
 
-interface CETHInterface {
+interface JAVAXInterface {
     function mint() external payable;
     function repayBorrow() external payable;
     function repayBorrowBehalf(address borrower) external payable;
-    function liquidateBorrow(address borrower, address cTokenCollateral) external payable;
+    function liquidateBorrow(address borrower, address jTokenCollateral) external payable;
 }
 
-interface ComptrollerInterface {
-    function enterMarkets(address[] calldata cTokens) external returns (uint[] memory);
-    function exitMarket(address cTokenAddress) external returns (uint);
+interface JoetrollerInterface {
+    function enterMarkets(address[] calldata jTokens) external returns (uint[] memory);
+    function exitMarket(address jTokenAddress) external returns (uint);
     function getAssetsIn(address account) external view returns (address[] memory);
     function getAccountLiquidity(address account) external view returns (uint, uint, uint);
-    function claimComp(address) external;
+    function claimJoe(address) external;
 }
 
-interface CompoundMappingInterface {
-    function cTokenMapping(string calldata tokenId) external view returns (address);
+interface JoeTraderMappingInterface {
+    function jTokenMapping(string calldata tokenId) external view returns (address);
     function getMapping(string calldata tokenId) external view returns (address, address);
 }
