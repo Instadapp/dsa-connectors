0702178e862863b0239dd051a26790b143f1266e
Croooook <rajatgoyal6404@gmail.com>
2022-02-16 11:19:56 +0530

    added joetroller contract address

--- contracts/avalanche/connectors/traderjoe/helpers.sol
+++ contracts/avalanche/connectors/traderjoe/helpers.sol
@@ -2,33 +2,33 @@ pragma solidity ^0.7.0;
 
 import { DSMath } from "../../common/math.sol";
 import { Basic } from "../../common/basic.sol";
-import { ComptrollerInterface, CompoundMappingInterface } from "./interface.sol";
+import { JoetrollerInterface, JoeTraderMappingInterface } from "./interface.sol";
 
 abstract contract Helpers is DSMath, Basic {
     /**
      * @dev Compound Comptroller
      */
-    ComptrollerInterface internal constant troller = ComptrollerInterface(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
+    JoetrollerInterface internal constant troller = JoetrollerInterface(0xdc13687554205E5b89Ac783db14bb5bba4A1eDaC);
 
     /**
      * @dev Compound Mapping
      */
-    CompoundMappingInterface internal constant compMapping = CompoundMappingInterface(0xe7a85d0adDB972A4f0A4e57B698B37f171519e88);
+    JoeTraderMappingInterface internal constant compMapping = JoeTraderMappingInterface(0xe7a85d0adDB972A4f0A4e57B698B37f171519e88);
 
     /**
      * @dev enter compound market
      */
-    function enterMarket(address cToken) internal {
+    function enterMarket(address jToken) internal {
         address[] memory markets = troller.getAssetsIn(address(this));
         bool isEntered = false;
         for (uint i = 0; i < markets.length; i++) {
-            if (markets[i] == cToken) {
+            if (markets[i] == jToken) {
                 isEntered = true;
             }
         }
         if (!isEntered) {
             address[] memory toEnter = new address[](1);
-            toEnter[0] = cToken;
+            toEnter[0] = jToken;
             troller.enterMarkets(toEnter);
         }
     }
