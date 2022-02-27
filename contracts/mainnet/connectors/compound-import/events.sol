58c2d3cb6eb5d6da1d62ddb34faaa5208b446558
Chinmay Chougaonkar <chinmay0402@gmail.com>
2022-02-27 09:40:25 +0530

    added LogCompoundImport event

--- contracts/mainnet/connectors/compound-import/events.sol
+++ contracts/mainnet/connectors/compound-import/events.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.7.6;
+pragma experimental ABIEncoderV2;
 
 contract Events {
-    
+    event LogCompoundImport(
+        address indexed user,
+        address[] ctokens,
+        string[] supplyIds,
+        string[] borrowIds,
+        uint[] supplyAmts,
+        uint[] borrowAmts
+    );
 }
