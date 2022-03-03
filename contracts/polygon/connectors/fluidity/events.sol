162f70ab5ec8e7bc13a898e72b5c36a7a2cb302e
CodeNinja04 <hemendrasharma2019@gmail.com>
2022-03-03 22:55:04 +0530

    params changed

--- contracts/polygon/connectors/fluidity/events.sol
+++ contracts/polygon/connectors/fluidity/events.sol
@@ -1,15 +1,15 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.6;
 
 
 contract Events {
 
-event LogSupply(address token_, uint amount_,uint itokenAmount_,uint getId,uint setId);
+event LogSupply(address indexed token_, uint amount_,uint itokenAmount_,uint getId,uint setId);
 
-event LogWithdraw(address token_, uint amt_,uint itokenAmount_,uint getId,uint setId);
+event LogWithdraw(address indexed token_, uint amt_,uint itokenAmount_,uint getId,uint setId);
 
-event LogWithdrawItoken(address token_, uint amt_,uint amount_,uint getId,uint setId);
+event LogWithdrawItoken(address indexed token_, uint amt_,uint amount_,uint getId,uint setId);
 
-event LogClaimReward(address user_, address token_,uint[] updatedRewards_)
+event LogClaimReward(address indexed user_ , address  indexed token_ , uint[] updatedRewards_)
 
 
 }
