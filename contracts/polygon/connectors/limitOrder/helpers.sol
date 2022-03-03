feca05639e33128ba83585633cc678b5ac7fff4e
Shriya Tyagi <47134275+shriyatyagii@users.noreply.github.com>
2022-03-04 03:35:17 +0530

    Update contracts/polygon/connectors/limitOrder/helpers.sol
    Co-authored-by: Samyak Jain <34437877+KaymasJain@users.noreply.github.com>

--- contracts/polygon/connectors/limitOrder/helpers.sol
+++ contracts/polygon/connectors/limitOrder/helpers.sol
@@ -55,7 +55,7 @@ contract Helpers is Basic {
         } else {
             amountSend_ = params_.amount == type(uint128).max ? getTokenBal(TokenInterface(params_.token1)) : params_.amount;
             convertMaticToWmatic(address(token1_) == wmaticAddr, token1_, amountSend_);
-            approve(TokenInterface(token1_), address(limitCon_), amountSend_);
+            approve(token1_, address(limitCon_), amountSend_);
         }
 
         {
