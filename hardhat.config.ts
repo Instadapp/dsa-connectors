68e007e26fbfddbbc152b773f8f663899bc53e6a
bhavik-m <bhavikmehta2001@gmail.com>
2022-03-12 22:24:50 +0530

    added connector on fanto,

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -36,36 +36,31 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API = process.env.ETHERSCAN_API_KEY;
 const POLYGONSCAN_API = process.env.POLYGON_API_KEY;
 const ARBISCAN_API = process.env.ARBISCAN_API_KEY;
-const OPTIMISM_API = process.env.OPTIMISM_API_KEY;
 const SNOWTRACE_API = process.env.SNOWTRACE_API_KEY;
-const mnemonic =
-  process.env.MNEMONIC ??
-  "test test test test test test test test test test test junk";
+const FANTOMSCAN_API = process.env.FANTOM_API_KEY;
+const mnemonic = process.env.MNEMONIC ?? "test test test test test test test test test test test junk";
 
 const networkGasPriceConfig: Record<string, string> = {
-  "mainnet": "160",
-  "polygon": "50",
-  "avalanche": "50",
-  "arbitrum": "2"
-}
+  mainnet: "160",
+  polygon: "50",
+  avalanche: "50",
+  arbitrum: "2"
+};
 
 function createConfig(network: string) {
   return {
     url: getNetworkUrl(network),
     accounts: !!PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : { mnemonic },
-    gasPrice: 1000000, // 0.0001 GWEI
+    gasPrice: 220000000000 // 0.0001 GWEI
   };
 }
 
 function getNetworkUrl(networkType: string) {
-  if (networkType === "avalanche")
-    return "https://api.avax.network/ext/bc/C/rpc";
-  else if (networkType === "polygon")
-    return `https://polygon-mainnet.g.alchemy.com/v2/${alchemyApiKey}`;
-  else if (networkType === "arbitrum")
-    return `https://arb-mainnet.g.alchemy.com/v2/${alchemyApiKey}`;
-  else if (networkType === "optimism")
-    return `https://opt-mainnet.g.alchemy.com/v2/${alchemyApiKey}`;
+  if (networkType === "avalanche") return "https://api.avax.network/ext/bc/C/rpc";
+  else if (networkType === "polygon") return `https://polygon-mainnet.g.alchemy.com/v2/${alchemyApiKey}`;
+  else if (networkType === "arbitrum") return `https://arb-mainnet.g.alchemy.com/v2/${alchemyApiKey}`;
+  else if (networkType === "optimism") return `https://opt-mainnet.g.alchemy.com/v2/${alchemyApiKey}`;
+  else if (networkType === "fantom") return `https://rpc.ftm.tools/`;
   else return `https://eth-mainnet.alchemyapi.io/v2/${alchemyApiKey}`;
 }
 
@@ -73,7 +68,7 @@ function getScanApiKey(networkType: string) {
   if (networkType === "avalanche") return SNOWTRACE_API;
   else if (networkType === "polygon") return POLYGONSCAN_API;
   else if (networkType === "arbitrum") return ARBISCAN_API;
-  else if (networkType === "optimism") return OPTIMISM_API;
+  else if (networkType === "fantom") return FANTOMSCAN_API;
   else return ETHERSCAN_API;
 }
 
@@ -88,53 +83,54 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
-          },
-        },
+            runs: 200
+          }
+        }
       },
       {
-        version: "0.6.0",
+        version: "0.6.0"
       },
       {
-        version: "0.6.2",
+        version: "0.6.2"
       },
       {
-        version: "0.6.5",
-      },
-    ],
+        version: "0.6.5"
+      }
+    ]
   },
   networks: {
     hardhat: {
       accounts: {
-        mnemonic,
+        mnemonic
       },
       chainId: chainIds.hardhat,
       forking: {
-        url: String(getNetworkUrl(String(process.env.networkType))),
-      },
+        url: String(getNetworkUrl(String(process.env.networkType)))
+      }
     },
     mainnet: createConfig("mainnet"),
     polygon: createConfig("polygon"),
     avalanche: createConfig("avalanche"),
     arbitrum: createConfig("arbitrum"),
     optimism: createConfig("optimism"),
+    fantom: createConfig("optimism")
   },
   paths: {
     artifacts: "./artifacts",
     cache: "./cache",
     sources: "./contracts",
-    tests: "./test",
+    tests: "./test"
   },
   etherscan: {
-    apiKey: getScanApiKey(String(process.env.networkType)),
+    apiKey: getScanApiKey(String(process.env.networkType))
   },
   typechain: {
     outDir: "typechain",
-    target: "ethers-v5",
+    target: "ethers-v5"
   },
   mocha: {
-    timeout: 10000 * 1000, // 10,000 seconds
-  },
+    timeout: 10000 * 1000 // 10,000 seconds
+  }
   // tenderly: {
   //   project: process.env.TENDERLY_PROJECT,
   //   username: process.env.TENDERLY_USERNAME,
