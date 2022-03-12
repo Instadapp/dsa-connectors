00e6f1677ffde4a7bfe66f39b56792d73819b2aa
bhavik-m <bhavikmehta2001@gmail.com>
2022-03-12 23:37:56 +0530

    updated storage variable to constant

--- contracts/mainnet/connectors/lido_stETH/helpers.sol
+++ contracts/mainnet/connectors/lido_stETH/helpers.sol
@@ -7,5 +7,6 @@ abstract contract Helpers {
 	ILido internal constant lidoInterface =
 		ILido(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
 
-	address treasury = 0x28849D2b63fA8D361e5fc15cB8aBB13019884d09; // Instadapp's treasury address
+	address internal constant treasury =
+		0x28849D2b63fA8D361e5fc15cB8aBB13019884d09; // Instadapp's treasury address
 }
