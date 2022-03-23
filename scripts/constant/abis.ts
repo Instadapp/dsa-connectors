c07b4676f5c3d1500b160abe8e049bf68123c2ed
pradyuman-verma <pradyumnverma27@gmail.com>
2022-03-24 01:12:02 +0530

    added instapool-c abis

--- scripts/constant/abis.ts
+++ scripts/constant/abis.ts
@@ -1,15 +1,16 @@
 export const abis: Record<string, any> = {
   core: {
     connectorsV2: require("./abi/core/connectorsV2.json"),
-    instaIndex: require("./abi/core/instaIndex.json"),
+    instaIndex: require("./abi/core/instaIndex.json")
   },
   connectors: {
     "Basic-v1": require("./abi/connectors/basic.json"),
     basic: require("./abi/connectors/basic.json"),
     auth: require("./abi/connectors/auth.json"),
     "INSTAPOOL-A": require("./abi/connectors/instapool.json"),
+    "INSTAPOOL-C": require("./abi/connectors/instapool-c.json")
   },
   basic: {
-    erc20: require("./abi/basics/erc20.json"),
-  },
+    erc20: require("./abi/basics/erc20.json")
+  }
 };
