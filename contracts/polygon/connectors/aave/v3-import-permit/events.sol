b39410963046137d67932db3a2dfacda68b7a45a
Richa <76250660+Richa-iitr@users.noreply.github.com>
2022-04-04 21:49:53 +0530

    minor change
    Co-authored-by: 0xPradyuman <63545809+pradyuman-verma@users.noreply.github.com>

--- contracts/polygon/connectors/aave/v3-import-permit/events.sol
+++ contracts/polygon/connectors/aave/v3-import-permit/events.sol
@@ -5,7 +5,7 @@ pragma experimental ABIEncoderV2;
 contract Events {
 	event LogAaveV3ImportWithPermit(
 		address indexed user,
-		address[] ctokens,
+		address[] atokens,
 		string[] supplyIds,
 		string[] borrowIds,
 		uint256[] flashLoanFees,
