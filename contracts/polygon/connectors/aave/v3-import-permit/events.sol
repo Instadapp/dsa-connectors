a131fb00174c247331d13bf1f002e127331136b6
Richa <76250660+Richa-iitr@users.noreply.github.com>
2022-04-04 21:48:01 +0530

    updated event name
    Co-authored-by: 0xPradyuman <63545809+pradyuman-verma@users.noreply.github.com>

--- contracts/polygon/connectors/aave/v3-import-permit/events.sol
+++ contracts/polygon/connectors/aave/v3-import-permit/events.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
 contract Events {
-	event LogAaveV3Import(
+	event LogAaveV3ImportWithPermit(
 		address indexed user,
 		address[] ctokens,
 		string[] supplyIds,
