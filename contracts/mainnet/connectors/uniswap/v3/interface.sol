b12e5231de01ee5c50153b15780ff7ed32eea10a
Richa-iitr <richa@cs.iitr.ac.in>
2022-04-28 22:15:07 +0530

    swap connectors for uniswapV3 mainnet

--- contracts/mainnet/connectors/uniswap/v3/interface.sol
+++ contracts/mainnet/connectors/uniswap/v3/interface.sol
@@ -36,6 +36,16 @@ interface ISwapRouter is IUniswapV3SwapCallback {
         uint256 amountOutMinimum;
         uint160 sqrtPriceLimitX96;
     }
+    struct ExactOutputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 deadline;
+        uint256 amountOut;
+        uint256 amountInMaximum;
+        uint160 sqrtPriceLimitX96;
+    }
 
     /// @notice Swaps `amountIn` of one token for as much as possible of another token
     /// @param params The parameters necessary for the swap, encoded as `ExactInputSingleParams` in calldata
@@ -60,17 +70,6 @@ interface ISwapRouter is IUniswapV3SwapCallback {
         external
         payable
         returns (uint256 amountOut);
-
-    struct ExactOutputSingleParams {
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        address recipient;
-        uint256 deadline;
-        uint256 amountOut;
-        uint256 amountInMaximum;
-        uint160 sqrtPriceLimitX96;
-    }
 
     /// @notice Swaps as little as possible of one token for `amountOut` of another token
     /// @param params The parameters necessary for the swap, encoded as `ExactOutputSingleParams` in calldata
