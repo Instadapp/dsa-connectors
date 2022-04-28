7f5f5f185b8ff067e7b7756d561bf37a553a2839
Richa-iitr <richa@cs.iitr.ac.in>
2022-04-29 00:30:32 +0530

    uniswapv3-swap connector-polygon

--- contracts/polygon/connectors/uniswap/v3/events.sol
+++ contracts/polygon/connectors/uniswap/v3/events.sol
@@ -40,4 +40,22 @@ contract Events {
     );
 
     event LogBurnPosition(uint256 tokenId);
+
+    event LogBuy(
+        address indexed buyToken,
+        address indexed sellToken,
+        uint256 buyAmt,
+        uint256 sellAmt,
+        uint256 getId,
+        uint256 setId
+    );
+
+    event LogSell(
+        address indexed buyToken,
+        address indexed sellToken,
+        uint256 buyAmt,
+        uint256 sellAmt,
+        uint256 getId,
+        uint256 setId
+    );
 }
