df7a6d52c3f0eda4e1e0e566ad0698860746dea2
Richa-iitr <richa@cs.iitr.ac.in>
2022-06-02 14:59:46 +0530

    tests-avalanche

--- test/avalanche/aave/v3-import-test.ts
+++ test/avalanche/aave/v3-import-test.ts
@@ -3,14 +3,14 @@ import hre, { ethers, network, waffle } from "hardhat";
 import type { Signer, Contract } from "ethers";
 import { BigNumber } from "bignumber.js";
 import { buildDSAv2 } from "../../../scripts/tests/buildDSAv2";
-import { addresses } from "../../../scripts/tests/arbitrum/addresses";
+import { addresses } from "../../../scripts/tests/avalanche/addresses";
 import { deployAndEnableConnector } from "../../../scripts/tests/deployAndEnableConnector";
 import { abis } from "../../../scripts/constant/abis";
 import { getMasterSigner } from "../../../scripts/tests/getMasterSigner";
 import { parseEther, parseUnits } from "ethers/lib/utils";
 import { encodeSpells } from "../../../scripts/tests/encodeSpells";
 import encodeFlashcastData from "../../../scripts/tests/encodeFlashcastData";
-import { ConnectV2AaveV3ImportArbitrum__factory, IERC20__factory } from "../../../typechain";
+import { ConnectV2AaveV3ImportAvalanche__factory, IERC20__factory } from "../../../typechain";
 import { connect } from "http2";
 const { provider } = waffle;
 
@@ -29,10 +29,11 @@ const ABI = [
 ];
 
 const aDaiAddress = "0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE"; 
-const aaveAddress = "0x794a61358D6845594F94dc1DB02A252b5b4814aD";
-let account = "0xc5ed2333f8a2c351fca35e5ebadb2a82f5d254c3";
-const DAI = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1";
-const USDC = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8";
+const aaveAddress = "0x794a61358d6845594f94dc1db02a252b5b4814ad";
+// const account = "0xf04adbf75cdfc5ed26eea4bbbb991db002036bdd";
+let account = "0x95eEA1Bdd19A8C40E9575048Dd0d6577D11a84e5";
+const DAI = "0xd586E7F844cEa2F87f50152665BCbc2C279D8d70";
+const ETH = "0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB";
 const connectorName = "AAVE-V3-IMPORT-X";
 let signer: any, wallet0: any;
 
@@ -212,11 +213,11 @@ const erc20Abi = [
 
 const token = new ethers.Contract(DAI, erc20Abi);
 const aDai = new ethers.Contract(aDaiAddress, ABI);
-const usdcToken = new ethers.Contract(USDC, erc20Abi);
+const wethToken = new ethers.Contract(ETH, erc20Abi);
 const aave = new ethers.Contract(aaveAddress, aaveAbi);
 const mnemonic = "test test test test test test test test test test test junk";
 
-describe("Import Aave v3 Position | Arbitrum", function () {
+describe("Import Aave v3 Position | Avalanche", function () {
   let dsaWallet0: any;
   let dsaWallet1: any;
   let dsaWallet2: any;
@@ -236,7 +237,7 @@ describe("Import Aave v3 Position | Arbitrum", function () {
           forking: {
             //@ts-ignore
             jsonRpcUrl: hre.config.networks.hardhat.forking.url,
-            blockNumber: 9333600
+            blockNumber: 13024200
           }
         }
       ]
@@ -252,12 +253,12 @@ describe("Import Aave v3 Position | Arbitrum", function () {
 
     signer = await ethers.getSigner(account);
 
-    await token.connect(signer).transfer(wallet0.address, ethers.utils.parseEther("10"));
+    await token.connect(signer).transfer(wallet0.address, ethers.utils.parseEther("8"));
 
     instaConnectorsV2 = await ethers.getContractAt(abis.core.connectorsV2, addresses.core.connectorsV2);
     connector = await deployAndEnableConnector({
       connectorName,
-      contractArtifact: ConnectV2AaveV3ImportArbitrum__factory,
+      contractArtifact: ConnectV2AaveV3ImportAvalanche__factory,
       signer: masterSigner,
       connectors: instaConnectorsV2
     });
@@ -267,24 +268,24 @@ describe("Import Aave v3 Position | Arbitrum", function () {
   describe("check user AAVE position", async () => {
     it("Should create Aave v3 position of DAI(collateral) and USDC(debt)", async () => {
       // approve DAI to aavePool
-      await token.connect(wallet0).approve(aaveAddress, parseEther("10"));
+      await token.connect(wallet0).approve(aaveAddress, parseEther("8"));
       console.log("Approve DAI on aave");
 
       //deposit DAI in aave
-      await aave.connect(wallet0).supply(DAI, parseEther("10"), wallet0.address, 3228);
+      await aave.connect(wallet0).supply(DAI, parseEther("8"), wallet0.address, 3228);
       console.log("Supplied DAI on aave");
 
-      //borrow USDC from aave
-      await aave.connect(wallet0).borrow(USDC, parseUnits("3", 6), 2, 3228, wallet0.address);
-      console.log("Borrowed USDC from aave");
+      //borrow ETH from aave
+      await aave.connect(wallet0).borrow(ETH, parseUnits("3", 6), 2, 3228, wallet0.address);
+      console.log("Borrowed ETH from aave");
     });
 
     it("Should check position of user", async () => {
       expect(await aDai.connect(wallet0).balanceOf(wallet0.address)).to.be.gte(
-        new BigNumber(10).multipliedBy(1e18).toString()
+        new BigNumber(8).multipliedBy(1e18).toString()
       );
 
-      expect(await usdcToken.connect(wallet0).balanceOf(wallet0.address)).to.be.gte(
+      expect(await wethToken.connect(wallet0).balanceOf(wallet0.address)).to.be.gte(
         new BigNumber(3).multipliedBy(1e6).toString()
       );
     });
@@ -333,20 +334,20 @@ describe("Import Aave v3 Position | Arbitrum", function () {
 
       walletBsigner = await ethers.getSigner(walletB.address);
 
-      await token.connect(signer).transfer(dsaWallet2.address, ethers.utils.parseEther("10"));
+      await token.connect(signer).transfer(dsaWallet2.address, ethers.utils.parseEther("8"));
 
       const spells = [
         //deposit DAI in aave
         {
           connector: "AAVE-V3-A",
           method: "deposit",
-          args: [DAI, parseEther("10"), 0, 0]
+          args: [DAI, parseEther("8"), 0, 0]
         },
         //borrow USDC from aave
         {
           connector: "AAVE-V3-A",
           method: "borrow",
-          args: [USDC, parseUnits("3", 6), 2, 0, 0]
+          args: [ETH, parseUnits("3", 6), 2, 0, 0]
         }
       ];
       const tx = await dsaWallet2.connect(walletBsigner).cast(...encodeSpells(spells), walletB.address);
@@ -355,10 +356,10 @@ describe("Import Aave v3 Position | Arbitrum", function () {
 
     it("Should check position of dsa", async () => {
       expect(await aDai.connect(walletBsigner).balanceOf(dsaWallet2.address)).to.be.gte(
-        new BigNumber(10).multipliedBy(1e18).toString()
+        new BigNumber(8).multipliedBy(1e18).toString()
       );
 
-      expect(await usdcToken.connect(walletBsigner).balanceOf(dsaWallet2.address)).to.be.gte(
+      expect(await wethToken.connect(walletBsigner).balanceOf(dsaWallet2.address)).to.be.gte(
         new BigNumber(3).multipliedBy(1e6).toString()
       );
     });
@@ -369,22 +370,21 @@ describe("Import Aave v3 Position | Arbitrum", function () {
       //Approving max amount
       const amount = ethers.constants.MaxUint256;
       const tx0 = await aDai.connect(wallet0).approve(dsaWallet0.address, amount);
-      const tx1 = await usdcToken.connect(wallet0).approve(dsaWallet0.address, amount);
-      const amount0 = new BigNumber(await usdcToken.connect(wallet0).balanceOf(wallet.address));
-      const amountB = new BigNumber(amount0.toString()).multipliedBy(5).dividedBy(1e4);
+      const tx1 = await wethToken.connect(wallet0).approve(dsaWallet0.address, amount);
+      const amount0 = new BigNumber(await wethToken.connect(wallet0).balanceOf(wallet.address));
+      const amountB = new BigNumber(amount0.toString()).multipliedBy(9).dividedBy(1e4);
       const amountWithFee = amount0.plus(amountB);
-      console.log(amount0);
 
       const flashSpells = [
         {
           connector: "AAVE-V3-IMPORT-X",
           method: "importAave",
-          args: [wallet.address, [[DAI], [USDC], false, [amountB.toFixed(0)]]]
+          args: [wallet.address, [[DAI], [ETH], false, [amountB.toFixed(0)]]]
         },
         {
           connector: "INSTAPOOL-C",
           method: "flashPayback",
-          args: [USDC, amountWithFee.toFixed(0), 0, 0]
+          args: [ETH, amountWithFee.toFixed(0), 0, 0]
         }
       ];
 
@@ -392,7 +392,7 @@ describe("Import Aave v3 Position | Arbitrum", function () {
         {
           connector: "INSTAPOOL-C",
           method: "flashBorrowAndCast",
-          args: [USDC, amount0.toString(), 5, encodeFlashcastData(flashSpells), "0x"]
+          args: [ETH, amount0.toString(), 1, encodeFlashcastData(flashSpells), "0x"]
         }
       ];
       const tx = await dsaWallet0.connect(wallet0).cast(...encodeSpells(spells), wallet.address);
@@ -401,7 +401,7 @@ describe("Import Aave v3 Position | Arbitrum", function () {
 
     it("Should check DSA-1 AAVE position", async () => {
       expect(await aDai.connect(wallet0).balanceOf(dsaWallet0.address)).to.be.gte(
-        new BigNumber(10).multipliedBy(1e18).toString()
+        new BigNumber(8).multipliedBy(1e18).toString()
       );
     });
 
@@ -410,20 +410,20 @@ describe("Import Aave v3 Position | Arbitrum", function () {
       const amount = ethers.constants.MaxUint256;
       const tx0 = await aDai.connect(walletBsigner).approve(dsaWallet1.address, amount);
 
-      const amount0 = new BigNumber(await usdcToken.connect(walletBsigner).balanceOf(dsaWallet2.address));
-      const amountB = new BigNumber(amount0.toString()).multipliedBy(5).dividedBy(1e4);
+      const amount0 = new BigNumber(await wethToken.connect(walletBsigner).balanceOf(dsaWallet2.address));
+      const amountB = new BigNumber(amount0.toString()).multipliedBy(9).dividedBy(1e4);
       const amountWithFee = amount0.plus(amountB);
 
       const flashSpells = [
         {
           connector: "AAVE-V3-IMPORT-X",
           method: "importAave",
-          args: [dsaWallet2.address, [[DAI], [USDC], false, [amountB.toFixed(0)]]] //dsaWallet2 --> DSA_A DSA with aave position
+          args: [dsaWallet2.address, [[DAI], [ETH], false, [amountB.toFixed(0)]]] //dsaWallet2 --> DSA_A DSA with aave position
         },
         {
           connector: "INSTAPOOL-C",
           method: "flashPayback",
-          args: [USDC, amountWithFee.toFixed(0), 0, 0]
+          args: [ETH, amountWithFee.toFixed(0), 0, 0]
         }
       ];
 
@@ -431,7 +431,7 @@ describe("Import Aave v3 Position | Arbitrum", function () {
         {
           connector: "INSTAPOOL-C",
           method: "flashBorrowAndCast",
-          args: [USDC, amount0.toString(), 5, encodeFlashcastData(flashSpells), "0x"]
+          args: [ETH, amount0.toString(), 1, encodeFlashcastData(flashSpells), "0x"]
         }
       ];
       //merge to dsaWallet1
@@ -441,7 +441,7 @@ describe("Import Aave v3 Position | Arbitrum", function () {
 
     it("Should check DSA-2 AAVE position", async () => {
       expect(await aDai.connect(wallet0).balanceOf(dsaWallet1.address)).to.be.gte(
-        new BigNumber(10).multipliedBy(1e18).toString()
+        new BigNumber(8).multipliedBy(1e18).toString()
       );
     });
   });
