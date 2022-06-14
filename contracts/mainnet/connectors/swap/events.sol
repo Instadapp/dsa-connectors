fb79ade8fea1fbd79a1fd80e025e18b3a6e6468a
Richa-iitr <richa@cs.iitr.ac.in>
2022-06-15 03:40:23 +0530

    minor change

--- contracts/mainnet/connectors/swap/events.sol
+++ contracts/mainnet/connectors/swap/events.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.7.0;
 pragma abicoder v2;
 
 contract Events {
-	event SwapAggregator(
+	event LogSwapAggregator(
 		string[] connectors,
 		string connectorName,
 		string eventName,
