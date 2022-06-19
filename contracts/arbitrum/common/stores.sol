a35ab9ec4b9de68413741523a95a9d8f9d0e5b7e
Richa-iitr <richa@cs.iitr.ac.in>
2022-06-19 21:06:51 +0530

    minor change

--- contracts/arbitrum/common/stores.sol
+++ contracts/arbitrum/common/stores.sol
@@ -29,7 +29,7 @@ abstract contract Stores {
   /**
    * @dev Return connectors registry address
    */
-	InstaConnectors internal constant instaConnectors = InstaConnectors(0x67fCE99Dd6d8d659eea2a1ac1b8881c57eb6592B);
+  InstaConnectors internal constant instaConnectors = InstaConnectors(0x67fCE99Dd6d8d659eea2a1ac1b8881c57eb6592B);
 
   /**
    * @dev Get Uint value from InstaMemory Contract.
