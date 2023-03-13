0ee4b2af830da227c7e72350c427b80c5ab564e1
sanchaymittal <sanchaymittal@gmail.com>
2023-03-13 16:02:29 +0700

    prettier

--- contracts/optimism/connectors/connext/helpers.sol
+++ contracts/optimism/connectors/connext/helpers.sol
@@ -8,17 +8,20 @@ import { IConnext } from "./interface.sol";
 import { IInstaReceiver } from "./interface.sol";
 
 contract Helpers is DSMath, Basic {
-  /**
-   * @dev Connext Diamond Address
-   */
-  address internal constant connextAddr = 0x8f7492DE823025b4CfaAB1D34c58963F2af5DEDA;
+	/**
+	 * @dev Connext Diamond Address
+	 */
+	address internal constant connextAddr =
+		0x8f7492DE823025b4CfaAB1D34c58963F2af5DEDA;
 	IConnext internal constant connext = IConnext(connextAddr);
 
-  /**
-   * @dev InstaReceiver Address
-   */
-	address internal constant instaReceiverAddr = 0x0000000000000000000000000000000000000000; // TODO: Add InstaReceiver address
-	IInstaReceiver internal constant instaReceiver = IInstaReceiver(instaReceiverAddr);
+	/**
+	 * @dev InstaReceiver Address
+	 */
+	address internal constant instaReceiverAddr =
+		0x0000000000000000000000000000000000000000; // TODO: Add InstaReceiver address
+	IInstaReceiver internal constant instaReceiver =
+		IInstaReceiver(instaReceiverAddr);
 
 	/**
 	 * @param destination The destination domain ID.
@@ -30,14 +33,14 @@ contract Helpers is DSMath, Basic {
 	 * @param callData Encoded calldata to send.
 	 */
 	struct XCallParams {
-    uint32 destination;
-    address to;
-    address asset;
-    address delegate;
-    uint256 amount;
-    uint256 slippage;
-    uint256 relayerFee;
-    bytes callData;
+		uint32 destination;
+		address to;
+		address asset;
+		address delegate;
+		uint256 amount;
+		uint256 slippage;
+		uint256 relayerFee;
+		bytes callData;
 	}
 
 	function _xcall(XCallParams memory params) internal {
@@ -49,7 +52,7 @@ contract Helpers is DSMath, Basic {
 				? address(this).balance
 				: params.amount;
 
-			// xcall does not take native asset, must wrap 
+			// xcall does not take native asset, must wrap
 			TokenInterface tokenContract = TokenInterface(wethAddr);
 			convertEthToWeth(true, tokenContract, params.amount);
 
@@ -70,7 +73,7 @@ contract Helpers is DSMath, Basic {
 		connext.xcall{ value: params.relayerFee + nativeTokenAmt }(
 			params.destination,
 			params.to,
-      params.asset,
+			params.asset,
 			params.delegate,
 			params.amount,
 			params.slippage,
