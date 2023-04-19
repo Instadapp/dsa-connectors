a51382d1dad4003c72de2f4e1ca379716e4e82b3
Shriya Tyagi <shriya243@gmail.com>
2023-04-19 20:10:27 +0400

    Added claimAndDelegateArbAirdrop + prettier

--- contracts/arbitrum/connectors/arb-claim/interface.sol
+++ contracts/arbitrum/connectors/arb-claim/interface.sol
@@ -19,21 +19,31 @@ interface IArbTokenContract {
 	function delegate(address delegatee) external;
 
 	function delegateBySig(
-        address delegatee,
-        uint256 nonce,
-        uint256 expiry,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external;
+		address delegatee,
+		uint256 nonce,
+		uint256 expiry,
+		uint8 v,
+		bytes32 r,
+		bytes32 s
+	) external;
 }
 
 interface TokenInterface {
-    function approve(address, uint256) external;
-    function transfer(address, uint) external;
-    function transferFrom(address, address, uint) external;
-    function deposit() external payable;
-    function withdraw(uint) external;
-    function balanceOf(address) external view returns (uint);
-    function decimals() external view returns (uint);
+	function approve(address, uint256) external;
+
+	function transfer(address, uint256) external;
+
+	function transferFrom(
+		address,
+		address,
+		uint256
+	) external;
+
+	function deposit() external payable;
+
+	function withdraw(uint256) external;
+
+	function balanceOf(address) external view returns (uint256);
+
+	function decimals() external view returns (uint256);
 }
