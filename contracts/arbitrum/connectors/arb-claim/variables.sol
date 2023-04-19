bade2b66426e74d1bfb58a789817f279548ed928
Shriya Tyagi <shriya243@gmail.com>
2023-04-19 20:06:43 +0400

    Add ARB token claim connector

---
+++ contracts/arbitrum/connectors/arb-claim/variables.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "./interface.sol";
+import { Basic } from "../../common/basic.sol";
+
+contract Variables is Basic {
+	IArbitrumTokenDistributor public constant ARBITRUM_TOKEN_DISTRIBUTOR =
+		IArbitrumTokenDistributor(0x67a24CE4321aB3aF51c2D0a4801c3E111D88C9d9);
+
+	IArbTokenContract public constant ARB_TOKEN_CONTRACT =
+		IArbTokenContract(0x912CE59144191C1204E64559FE8253a0e49E6548);
+
+	struct SignedPermits {
+		uint8 v;
+		bytes32 r;
+		bytes32 s;
+		uint256 expiry;
+	}
+}
