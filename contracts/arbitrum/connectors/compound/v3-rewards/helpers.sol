165b09e45cb8b2124170c1b32daa0f1578ed7a73
q1q0 <carloslin9999@gmail.com>
2023-05-24 09:13:56 -0400

    add compound v3 reward

---
+++ contracts/arbitrum/connectors/compound/v3-rewards/helpers.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+pragma abicoder v2;
+
+import { TokenInterface } from "../../../common/interfaces.sol";
+import { DSMath } from "../../../common/math.sol";
+import { Basic } from "../../../common/basic.sol";
+import {  CometRewards } from "./interface.sol";
+
+abstract contract Helpers is DSMath, Basic {
+	CometRewards internal constant cometRewards =
+		CometRewards(0x88730d254A2f7e6AC8388c3198aFd694bA9f7fae);
+}
