77ab33a14036359bd5689a88327f33f06cfdf783
Shriya Tyagi <shriya243@gmail.com>
2023-11-26 21:44:24 +0530

    update version

--- contracts/mainnet/connectors/aave/v3/main.sol
+++ contracts/mainnet/connectors/aave/v3/main.sol
@@ -517,5 +517,5 @@ abstract contract AaveResolver is Events, Helpers {
 }
 
 contract ConnectV2AaveV3 is AaveResolver {
-	string public constant name = "AaveV3-v1.0";
+	string public constant name = "AaveV3-v1.1";
 }
