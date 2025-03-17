64a6972467cc0972e8aa9e807b3a681a658a1795
Samarendra Gouda <samarendragouda@gmail.com>
2025-03-17 23:36:33 +0530

    feat: added mainnet import with permit contract

---
+++ contracts/mainnet/connectors/aave/v3-import-permit/events.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+contract Events {
+	event LogAaveV3ImportWithPermit(
+		address indexed user,
+		address[] atokens,
+		string[] supplyIds,
+		string[] borrowIds,
+		uint256[] flashLoanFees,
+		uint256[] supplyAmts,
+		uint256[] borrowAmts
+	);
+	event LogAaveV3ImportWithPermitAndCollateral(
+		address indexed user,
+		address[] atokens,
+		string[] supplyIds,
+		string[] borrowIds,
+		uint256[] flashLoanFees,
+		uint256[] supplyAmts,
+		uint256[] borrowAmts,
+		bool[] enableCollateral
+	);
+}
